bdbe62b6cd7672b48e9f98285715e33be563a5c6
jayachandrands <jaidurai1422@gmail.com>
2022-06-09 11:14:59 -0700

    task09

---
+++ grade.js
@@ -0,0 +1,12 @@
+let marks = 75;
+if(50>marks){
+    console.log("student grade is E");
+}else if(60>marks){
+    console.log("student grade is D");
+}else if(80>marks){
+    console.log("student grade is C");
+}else if(90>marks){
+    console.log("student grade is B");
+}else if(100>marks){
+    console.log("student grade is A");
+}
